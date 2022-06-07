870fb6986a17c272b7b149e9a7bcbcb2b16ff922
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-06-07 09:41:51 +0200

    Update delete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://kodilla-tasks-mw.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://kodilla-tasks-mw.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://kodilla-tasks-mw.herokuapp.com//v1/task/';
+   const trelloApiRoot = 'https://kodilla-tasks-mw.herokuapp.com//v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
@@ -108,12 +108,10 @@ $(document).ready(function() {
    function handleTaskDeleteRequest() {
       var parentEl = $(this).parents('[data-task-id]');
       var taskId = parentEl.attr('data-task-id');
-      var requestUrl = apiRoot + 'deleteTask';
+      var requestUrl = apiRoot + taskId;
 
       $.ajax({
-         url: requestUrl + '/?' + $.param({
-            taskId: taskId
-         }),
+         url: requestUrl, 
          method: 'DELETE',
          success: function() {
             parentEl.slideUp(400, function() { parentEl.remove(); });
