ef8aa0eb5e73cc990c48241f9ff245b357043628
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-06-14 09:51:58 +0200

    Update DELETE

--- script.js
+++ script.js
@@ -108,10 +108,10 @@ $(document).ready(function() {
    function handleTaskDeleteRequest() {
       var parentEl = $(this).parents('[data-task-id]');
       var taskId = parentEl.attr('data-task-id');
-      var requestUrl = apiRoot + tasks;
+      var requestUrl = apiRoot + 'tasks';
 
       $.ajax({
-         url: requestUrl + '/' + id, 
+         url: requestUrl + '/' + taskId, 
          method: 'DELETE',
          success: function() {
             parentEl.slideUp(400, function() { parentEl.remove(); });
